b0f55f5381fa26b287fd8a4660eba6fa180f4ec0
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-14 16:08:01 +0600

    some task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Blogs from "./components/blogs/Blogs";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Blogs></Blogs>
     </>
   );
 }
