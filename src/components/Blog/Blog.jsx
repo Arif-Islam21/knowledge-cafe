5f54f19ae2fb4f2ba70fd31507e486fd2c1e2147
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-14 22:23:06 +0600

    prop drilling

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -23,6 +23,11 @@ const Blog = ({ blog }) => {
           <div>
             <h3 className="text-2xl">{author}</h3>
             <p>{posted_date}</p>
+            {hashtags.map((tag, idx) => (
+              <span key={idx}>
+                <a href="">{tag}</a>
+              </span>
+            ))}
           </div>
         </div>
         <div>
